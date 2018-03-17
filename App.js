19cba2f02bf4f3894d2c8bd3d294e99f65da9a61
Jacob A. Johnson <johnson3250@gmail.com>
2018-03-16 23:11:10 -0500

    Moving along now have added a new package react-navigation to project for easy navigation.

--- App.js
+++ App.js
@@ -1,46 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+import { StackNavigator} from 'react-navigation';
+import HomeScreen from './views/homeView/home';
+
+
+const AppNavigation = StackNavigator({
+  Home: {
+    screen: HomeScreen
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <ImageBackground 
-          source={require('./images/wineBackground.jpg')} 
-          style={{
-            width: '100%', 
-            height: '100%', 
-            alignItems: 'center', 
-          }}>
-          <View style={styles.container}>
-            <Text style={styles.heading}>Wine Cellar</Text>
-            <View style={styles.buttonBox}>
-              <Button style={{marginRight: 10, flex: 1,}} onPress={()=>{}} title="Profile"/>
-              <Button style={{marginRight: 10, flex: 1,}} onPress={()=>{}} title="Wine Cellar"/>
-              <Button style={{ flex: 1,}} onPress={()=>{}} title="Add Wine"/>
-            </View>
-          </View>
-        </ImageBackground>
-      </View>
+      <AppNavigation/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  heading:{
-    fontSize: 40,
-    color: '#ffffff',
-  },
-  buttonBox:{
-    width: '75%',
-    paddingBottom: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  container: {
-    width: '100%',
-    backgroundColor: 'rgba(0,0,0,.5)',
-    alignItems: 'center',
-    paddingTop: 50,
-  },
-});
---
+++ views/homeView/home.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: 'black',
+    },
+    headerTitleStyle: {
+      color: '#fff',
+    },
+  }
+  render() {
+    return (
+      <View>
+        <ImageBackground 
+          source={require('../../images/wineBackground.jpg')} 
+          style={{
+            width: '100%', 
+            height: '100%', 
+            alignItems: 'center', 
+          }}>
+          <View style={styles.container}>
+            <Text style={styles.heading}>Wine Cellar</Text>
+            <View style={styles.buttonBox}>
+              <Button style={{marginRight: 10, flex: 1,}} onPress={()=>{}} title="Profile"/>
+              <Button style={{marginRight: 10, flex: 1,}} onPress={()=>{}} title="Wine Cellar"/>
+              <Button style={{ flex: 1,}} onPress={()=>{}} title="Add Wine"/>
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  heading:{
+    fontSize: 40,
+    color: '#ffffff',
+  },
+  buttonBox:{
+    width: '75%',
+    paddingBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  container: {
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,.5)',
+    alignItems: 'center',
+    paddingTop: 50,
+  },
+});
