9d19e53ff7f4e0d9be05ff8887d1cf016abc6e45
Jacob A. Johnson <johnson3250@gmail.com>
2018-03-29 21:06:18 -0500

    next set of style for the table.

--- shared/tables/table.js
+++ shared/tables/table.js
@@ -7,16 +7,16 @@ export default class Table extends React.Component {
     renderRow = (index) => {
         return (
             <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', width: '100%' }} key={index}>
-                <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'red', marginVertical: '2%', marginLeft: '2%' }} key="1">    
+                <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'red', marginVertical: '2%', marginLeft: '2%', padding: '2%' }} key="1">    
                     <Text style={{ color: 'white' }}>1</Text>
                 </View>
-                <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'blue', marginVertical: '2%' }} key="2">
+                <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'blue', marginVertical: '2%', padding: '2%' }} key="2">
                     <Text style={{ color: 'white' }}>2</Text>
                 </View>
-                <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'green', marginVertical: '2%' }} key="3">
+                <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'green', marginVertical: '2%', padding: '2%' }} key="3">
                     <Text style={{ color: 'white' }}>3</Text>
                 </View>
-                <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'purple', marginVertical: '2%', marginRight:'2%' }} key="4">
+                <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: 'purple', marginVertical: '2%', marginRight:'2%', padding: '2%' }} key="4">
                     <Text style={{ color: 'white' }}>4</Text>
                 </View>
             </View>
